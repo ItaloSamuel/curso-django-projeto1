1c4dfa756de051eb6e5ecb23568dd9b4e7b6fc06
Ítalo Samuel <italosdjr123@gmail.com>
2023-11-27 15:46:15 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Ítalo Samuel'
-    })
-
+    return render(request, 'recipes/pages/home.html')
