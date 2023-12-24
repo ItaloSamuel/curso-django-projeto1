4643e32a100654ac2c4d3afac2d827105b1cfeee
Ítalo Samuel <italosdjr123@gmail.com>
2023-12-24 15:37:16 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from recipes.models import Recipe
@@ -11,12 +12,18 @@ def home(request):
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id,
         is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    
+    if not recipes:
+        raise Http404('Not Found')
+    
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 
