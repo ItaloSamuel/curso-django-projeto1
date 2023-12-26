768bf47c1268f5ec7e037ed58024465868116047
Ítalo Samuel <italosdjr123@gmail.com>
2023-12-26 14:42:04 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,6 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
 
 
 def home(request):
@@ -14,12 +12,9 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
-        is_published=True).order_by('-id')
-    
-    if not recipes:
-        raise Http404('Not Found')
+        is_published=True).order_by('-id'))
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
@@ -28,8 +23,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
  
